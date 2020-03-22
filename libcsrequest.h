c2b61f66a91780962dd4902d6b91f3c6ffc7ef64
wdonadelli <wdonadelli@gmail.com>
2020-03-22 02:07:32 -0300

    Em processo de criação...

--- libcsrequest.h
+++ libcsrequest.h
@@ -71,25 +71,25 @@ SOFTWARE.
 	typedef struct
 	{
 		/*-- Métodos --*/
-		int (*sql)();		/* executa uma instrução SQL */
-		int (*insert)();	/* constrói uma instrução INSERT (ver método add) */
-		int (*update)();	/* constrói uma instrução UPDATE (ver método add) */
-		int (*delete)();	/* constrói uma instrução DELETE (ver método add) */
-		int (*select)();	/* constrói uma instrução SELECT (ver método add) */
-		int (*add)();		/* registra informações para contrutores acima */
-		int (*clear)();	/* apaga registros (ver método add) */
-		char *(*fetch)();	/* retorna o valor da coluna (ver método sql) */
-		void (*reader)();	/* guarda a função de pesquisa */
+		int (*sql)();     /* executa uma instrução SQL */
+		int (*insert)();  /* constrói uma instrução INSERT (ver método add) */
+		int (*update)();  /* constrói uma instrução UPDATE (ver método add) */
+		int (*delete)();  /* constrói uma instrução DELETE (ver método add) */
+		int (*select)();  /* constrói uma instrução SELECT (ver método add) */
+		int (*add)();     /* registra informações para contrutores acima */
+		int (*clear)();   /* apaga registros (ver método add) */
+		char *(*fetch)(); /* retorna o valor da coluna (ver método sql) */
+		void (*reader)(); /* guarda a função de pesquisa */
 		
 		/*-- Atributos --*/
-		char *file;					/* caminho para o banco de dados */
-		unsigned int error: 1;	/* registra erro na última operação */
-		char *msg;					/* registra mensagem de erro da última operação */
-		unsigned long int row;	/* guarda o número da linha */
-		unsigned int len;			/* guarda o número de colunas */
-		char **col;					/* guarda o array com nomes das colunas */
-		char **val;					/* guarda o array com valores das colunas */
-		csrData *data;				/* guarda a estrutura de registros */
+		char *file;            /* caminho para o banco de dados */
+		unsigned int error: 1; /* registra erro na última operação */
+		char *msg;             /* registra mensagem de erro da última operação */
+		unsigned long int row; /* guarda o número da linha */
+		unsigned int len;      /* guarda o número de colunas */
+		char **col;            /* guarda o array com nomes das colunas */
+		char **val;            /* guarda o array com valores das colunas */
+		csrData *data;         /* guarda a estrutura de registros */
 		
 	} csrObject;
 	
@@ -110,12 +110,12 @@ SOFTWARE.
 -----------------------------------------------------------------------------*/
 	int __csr_sql__ (csrObject *self, char *query, void (*reader)());
 
-	#define __CSR_SQL__(SELF)												\
-		int __csr_sql__##SELF (char *query, void (*reader)())		\
-		{																			\
-			return __csr_sql__(&SELF, query, reader);					\
-		}																			\
-		SELF.sql = __csr_sql__##SELF;										\
+	#define __CSR_SQL__(SELF)                                \
+		int __csr_sql__##SELF (char *query, void (*reader)()) \
+		{                                                     \
+			return __csr_sql__(&SELF, query, reader);          \
+		}                                                     \
+		SELF.sql = __csr_sql__##SELF;                         \
 
 /*-----------------------------------------------------------------------------
 	__csr_insert__ () constrói uma instrução INSERT e a executa a partir das
@@ -125,12 +125,12 @@ SOFTWARE.
 -----------------------------------------------------------------------------*/
 	int __csr_insert__ (csrObject *self, char *table);
 
-	#define __CSR_INSERT__(SELF)						\
-		int __csr_insert__##SELF (char *table)		\
-		{														\
-			return __csr_insert__(&SELF, table);	\
-		}														\
-		SELF.insert = __csr_insert__##SELF;			\
+	#define __CSR_INSERT__(SELF)               \
+		int __csr_insert__##SELF (char *table)  \
+		{                                       \
+			return __csr_insert__(&SELF, table); \
+		}                                       \
+		SELF.insert = __csr_insert__##SELF;     \
 
 /*-----------------------------------------------------------------------------
 	__csr_update__ () constrói uma instrução UPDATE e a executa a partir das
@@ -140,12 +140,12 @@ SOFTWARE.
 -----------------------------------------------------------------------------*/
 	int __csr_update__ (csrObject *self, char *table);
 
-	#define __CSR_UPDATE__(SELF)						\
-		int __csr_update__##SELF (char *table)		\
-		{														\
-			return __csr_update__(&SELF, table);	\
-		}														\
-		SELF.update = __csr_update__##SELF;			\
+	#define __CSR_UPDATE__(SELF)               \
+		int __csr_update__##SELF (char *table)  \
+		{                                       \
+			return __csr_update__(&SELF, table); \
+		}                                       \
+		SELF.update = __csr_update__##SELF;     \
 
 /*-----------------------------------------------------------------------------
 	__csr_delete__ () constrói uma instrução DELETE e a executa a partir das
@@ -155,12 +155,12 @@ SOFTWARE.
 -----------------------------------------------------------------------------*/
 	int __csr_delete__ (csrObject *self, char *table);
 
-	#define __CSR_DELETE__(SELF)						\
-		int __csr_delete__##SELF (char *table)		\
-		{														\
-			return __csr_delete__(&SELF, table);	\
-		}														\
-		SELF.delete = __csr_delete__##SELF;			\
+	#define __CSR_DELETE__(SELF)               \
+		int __csr_delete__##SELF (char *table)  \
+		{                                       \
+			return __csr_delete__(&SELF, table); \
+		}                                       \
+		SELF.delete = __csr_delete__##SELF;     \
 
 /*-----------------------------------------------------------------------------
 	__csr_select__ () constrói uma instrução SELECT e a executa a partir das
@@ -171,12 +171,12 @@ SOFTWARE.
 -----------------------------------------------------------------------------*/
 	int __csr_select__ (csrObject *self, char *table, void (*reader)());
 
-	#define __CSR_SELECT__(SELF)												\
-		int __csr_select__##SELF (char *table, void (*reader)())		\
-		{																				\
-			return __csr_select__(&SELF, table, reader);					\
-		}																				\
-		SELF.select = __csr_select__##SELF;									\
+	#define __CSR_SELECT__(SELF)                                \
+		int __csr_select__##SELF (char *table, void (*reader)()) \
+		{                                                        \
+			return __csr_select__(&SELF, table, reader);          \
+		}                                                        \
+		SELF.select = __csr_select__##SELF;                      \
 
 /*-----------------------------------------------------------------------------
 	__csr_add__ () adiciona registros para execução dos métodos construtores de
@@ -188,12 +188,12 @@ SOFTWARE.
 -----------------------------------------------------------------------------*/
 	int __csr_add__ (csrObject *self, char *column, char *value, int where);
 
-	#define __CSR_ADD__(SELF)														\
-		int __csr_add__##SELF (char *column, char *value, int where)	\
-		{																					\
-			return __csr_add__(&SELF, column, value, where);				\
-		}																					\
-		SELF.add = __csr_add__##SELF;												\
+	#define __CSR_ADD__(SELF)                                       \
+		int __csr_add__##SELF (char *column, char *value, int where) \
+		{                                                            \
+			return __csr_add__(&SELF, column, value, where);          \
+		}                                                            \
+		SELF.add = __csr_add__##SELF;                                \
 
 /*-----------------------------------------------------------------------------
 	__csr_clear__ () limpa os dados adicionados pelo método add
@@ -201,14 +201,12 @@ SOFTWARE.
 -----------------------------------------------------------------------------*/
 	int __csr_clear__ (csrObject *self);
 
-	#define __CSR_CLEAR__(SELF)				\
-		int __csr_clear__##SELF ()				\
-		{												\
-			return __csr_clear__(&SELF);		\
-		}												\
-		SELF.clear = __csr_clear__##SELF;	\
-
-
+	#define __CSR_CLEAR__(SELF)          \
+		int __csr_clear__##SELF ()        \
+		{                                 \
+			return __csr_clear__(&SELF);   \
+		}                                 \
+		SELF.clear = __csr_clear__##SELF; \
 
 /*-----------------------------------------------------------------------------
 	__csr_clear__ () limpa os dados adicionados pelo método add
@@ -216,12 +214,12 @@ SOFTWARE.
 -----------------------------------------------------------------------------*/
 	int __csr_clear__ (csrObject *self);
 
-	#define __CSR_CLEAR__(SELF)				\
-		int __csr_clear__##SELF ()				\
-		{												\
-			return __csr_clear__(&SELF);		\
-		}												\
-		SELF.clear = __csr_clear__##SELF;	\
+	#define __CSR_CLEAR__(SELF)          \
+		int __csr_clear__##SELF ()        \
+		{                                 \
+			return __csr_clear__(&SELF);   \
+		}                                 \
+		SELF.clear = __csr_clear__##SELF; \
 
 /*-----------------------------------------------------------------------------
 	__csr_fetch__ () retorna a string com o valor da coluna na instrução SELECT
@@ -229,39 +227,39 @@ SOFTWARE.
 -----------------------------------------------------------------------------*/
 	char *__csr_fetch__ (csrObject *self, char *col);
 
-	#define __CSR_FETCH__(SELF)					\
-		char *__csr_fetch__##SELF (char *col)	\
-		{													\
-			return __csr_fetch__(&SELF, col);	\
-		}													\
-		SELF.fetch = __csr_fetch__##SELF;		\
+	#define __CSR_FETCH__(SELF)              \
+		char *__csr_fetch__##SELF (char *col) \
+		{                                     \
+			return __csr_fetch__(&SELF, col);  \
+		}                                     \
+		SELF.fetch = __csr_fetch__##SELF;     \
 
 /*-----------------------------------------------------------------------------
 	new_csr () construtor da estrutura
 -----------------------------------------------------------------------------*/
-	#define new_csr(OBJECT, FILE)												\
-																						\
-		csrObject OBJECT;															\
-		OBJECT.file = malloc ((strlen(FILE) + 1) * sizeof (char));	\
-		OBJECT.msg  = malloc (2 * sizeof (char));							\
-		strcpy(OBJECT.file, FILE);												\
-		strcpy(OBJECT.msg, "");													\
-		OBJECT.error  = 0;														\
-		OBJECT.row    = 0;														\
-		OBJECT.len    = 0;														\
-		OBJECT.data   = NULL;													\
-		OBJECT.col    = NULL;													\
-		OBJECT.val    = NULL;													\
-		OBJECT.reader = NULL;													\
-																						\
-		__CSR_SQL__(OBJECT);														\
-		__CSR_INSERT__(OBJECT);													\
-		__CSR_UPDATE__(OBJECT);													\
-		__CSR_DELETE__(OBJECT);													\
-		__CSR_SELECT__(OBJECT);													\
-		__CSR_ADD__(OBJECT);														\
-		__CSR_CLEAR__(OBJECT);													\
-		__CSR_FETCH__(OBJECT);													\
+	#define new_csr(OBJECT, FILE)                                 \
+                                                                 \
+		csrObject OBJECT;                                          \
+		OBJECT.file = malloc ((strlen(FILE) + 1) * sizeof (char)); \
+		OBJECT.msg  = malloc (2 * sizeof (char));                  \
+		strcpy(OBJECT.file, FILE);                                 \
+		strcpy(OBJECT.msg, "");                                    \
+		OBJECT.error  = 0;                                         \
+		OBJECT.row    = 0;                                         \
+		OBJECT.len    = 0;                                         \
+		OBJECT.data   = NULL;                                      \
+		OBJECT.col    = NULL;                                      \
+		OBJECT.val    = NULL;                                      \
+		OBJECT.reader = NULL;                                      \
+                                                                 \
+		__CSR_SQL__(OBJECT);                                       \
+		__CSR_INSERT__(OBJECT);                                    \
+		__CSR_UPDATE__(OBJECT);                                    \
+		__CSR_DELETE__(OBJECT);                                    \
+		__CSR_SELECT__(OBJECT);                                    \
+		__CSR_ADD__(OBJECT);                                       \
+		__CSR_CLEAR__(OBJECT);                                     \
+		__CSR_FETCH__(OBJECT);                                     \
 
 
 #endif
